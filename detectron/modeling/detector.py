b13d524c40ffb1b3c84ae968e5d5a2561f9f0cab
caiPactera <jiaqi.cai22@pactera.com>
2018-06-21 11:47:05 -0700

    bug fix

--- detectron/modeling/detector.py
+++ detectron/modeling/detector.py
@@ -299,7 +299,7 @@ class DetectionModelHelper(cnn.CNNModelHelper):
                 sampling_ratio=sampling_ratio
             )
         # Only return the first blob (the transformed features)
-        pprint(xform_out[0] if isinstance(xform_out, tuple) else xform_out
+        pprint(xform_out[0] if isinstance(xform_out, tuple) else xform_out)
         return xform_out[0] if isinstance(xform_out, tuple) else xform_out
 
     def ConvShared(
