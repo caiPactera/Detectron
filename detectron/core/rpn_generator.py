6cf23678aa4a7ecb06dc1ba5cb1d418042f4e911
caiPactera <jiaqi.cai22@pactera.com>
2018-06-26 11:04:30 -0700

    print rpn feature

--- detectron/core/rpn_generator.py
+++ detectron/core/rpn_generator.py
@@ -228,7 +228,8 @@ def im_proposals(model, im):
             core.ScopedName('conv_rpn_fpn' + str(l))
             for l in range(k_min, k_max + 1)
         ]
-        print(roi_fpn_inputs)
+        features = workspace.FetchBlob(core.ScopedName(roi_fpn_inputs[0]))
+        print(features)
         score_names = [
             core.ScopedName('rpn_roi_probs_fpn' + str(l))
             for l in range(k_min, k_max + 1)
