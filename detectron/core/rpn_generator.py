f1011ae1846657eb565bd640b02511f68246a46e
caiPactera <jiaqi.cai22@pactera.com>
2018-06-26 18:23:47 -0700

    fix print

--- detectron/core/rpn_generator.py
+++ detectron/core/rpn_generator.py
@@ -238,7 +238,7 @@ def im_proposals(model, im):
                 for k in j:
                     out.write('[')
                     for l in k:
-                        out.write('%d, ' % features[i,j,k,l])
+                        out.write('%d, ' % features[i][j][k][l])
                     out.write(']\n')
                 out.write(']\n')
             out.write(']\n')
