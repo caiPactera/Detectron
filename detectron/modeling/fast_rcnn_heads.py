dc6f3e01517a21e8be230968eff5c983bb153953
caiPactera <jiaqi.cai22@pactera.com>
2018-07-17 18:15:44 -0700

    softmax focal

--- detectron/modeling/fast_rcnn_heads.py
+++ detectron/modeling/fast_rcnn_heads.py
@@ -78,28 +78,28 @@ def add_fast_rcnn_losses(model):
     #     ['cls_score', 'labels_int32'], ['cls_prob', 'loss_cls'],
     #     scale=model.GetLossScale()
     # )
-    loss_cls = model.net.SigmoidFocalLoss(
-                [
-                    'cls_score', 'labels_int32',
-                    'fg_num'
-                ],
-                ['loss_cls'],
-                gamma=cfg.RETINANET.LOSS_GAMMA,
-                alpha=cfg.RETINANET.LOSS_ALPHA,
-                scale=model.GetLossScale(),
-                num_classes=model.num_classes - 1
-            )
-    # loss_cls, cls_prob = model.net.SoftmaxFocalLoss(
+    # loss_cls = model.net.SigmoidFocalLoss(
     #             [
     #                 'cls_score', 'labels_int32',
     #                 'fg_num'
     #             ],
-    #             ['loss_cls', 'cls_prob'],
+    #             ['loss_cls'],
     #             gamma=cfg.RETINANET.LOSS_GAMMA,
     #             alpha=cfg.RETINANET.LOSS_ALPHA,
     #             scale=model.GetLossScale(),
-    #             num_classes=model.num_classes
+    #             num_classes=model.num_classes - 1
     #         )
+    loss_cls, cls_prob = model.net.SoftmaxFocalLoss(
+                [
+                    'cls_score', 'labels_int32',
+                    'fg_num'
+                ],
+                ['loss_cls', 'cls_prob'],
+                gamma=cfg.RETINANET.LOSS_GAMMA,
+                alpha=cfg.RETINANET.LOSS_ALPHA,
+                scale=model.GetLossScale(),
+                num_classes=model.num_classes
+            )
 
     loss_bbox = model.net.SmoothL1Loss(
         [
@@ -110,9 +110,9 @@ def add_fast_rcnn_losses(model):
         scale=model.GetLossScale()
     )
     loss_gradients = blob_utils.get_loss_gradients(model, [loss_cls, loss_bbox])
-    # model.Accuracy(['cls_prob', 'labels_int32'], 'accuracy_cls')
+    model.Accuracy(['cls_prob', 'labels_int32'], 'accuracy_cls')
     model.AddLosses(['loss_cls', 'loss_bbox'])
-    # model.AddMetrics('accuracy_cls')
+    model.AddMetrics('accuracy_cls')
     return loss_gradients
 
 
